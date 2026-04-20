9a2322d09b5d90717dbd6449f16da513296b585a
Trenton Edwards <gage@tg11.org>
2026-04-19 20:33:14 -0400

    Copilot Use for Transfer from Daphne management to a Docker based system for ease of use and portability part 5

--- boundless/settings.py
+++ boundless/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,9 +139,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,7 +10,8 @@ echo "==> Running migrations…"
 python manage.py migrate --noinput
 
 echo "==> Collecting static files…"
-python manage.py collectstatic --noinput
+rm -rf /app/staticfiles/*
+python manage.py collectstatic --noinput --clear
 
 echo "==> Starting Daphne (ASGI)…"
 exec daphne -b 0.0.0.0 -p 8000 boundless.asgi:application
